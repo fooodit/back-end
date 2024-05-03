b2fde63fcefe688e9fe7e13ffaa8b98f66d7b170
wiishj <inging007@naver.com>
2024-05-04 07:16:25 +0900

    update writer, hoursAgo

--- src/main/java/hack/foodit/domain/post/entity/dto/PostResponseDTO.java
+++ src/main/java/hack/foodit/domain/post/entity/dto/PostResponseDTO.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Column;
 import lombok.*;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Getter
@@ -15,21 +18,26 @@ import java.util.List;
 @AllArgsConstructor
 public class PostResponseDTO {
     Long id;
+    String writer;
     String title;
     String content;
     Long likeCount;
     Long unlikeCount;
+    Integer hoursAgo;
     List<String> imageUrls;
     List<Integer> categoryList;
     LocalDateTime createdAt;
     LocalDateTime modifiedAt;
+
     public static PostResponseDTO from(Post entity){
         return PostResponseDTO.builder()
                 .id(entity.getId())
+                .writer(entity.getMember().getName())
                 .title(entity.getTitle())
                 .content(entity.getContent())
                 .likeCount(entity.getLikeCount())
                 .unlikeCount(entity.getUnlikeCount())
+                .hoursAgo((int)Duration.between(entity.getCreatedAt(),LocalDateTime.now()).toHours())
                 .imageUrls(entity.getImageUrlList())
                 .categoryList(entity.getCategoryList())
                 .createdAt(entity.getCreatedAt())
