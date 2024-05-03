d5f1620a611d2f2cca924cdf2c22db90e0e7f175
seokhee <skdlzl753@naver.com>
2024-05-04 00:28:30 +0900

    init entity

--- src/main/java/hack/foodit/domain/post/entity/Post.java
+++ src/main/java/hack/foodit/domain/post/entity/Post.java
@@ -4,7 +4,6 @@ import hack.foodit.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
